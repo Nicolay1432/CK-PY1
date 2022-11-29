43c5ea025a37512b4818725c7fd612a94ab15f71
Nicolay1432 <119425398+Nicolay1432@users.noreply.github.com>
2022-11-29 17:57:10 +0300

    Лабораторная работа 1

---
+++ task_3.py
@@ -0,0 +1,13 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+money_capital = 0  # количество денег, чтобы прожить 10 месяцев
+i = 0
+while i <= months - 1:    # первый месяц не учитывается
+    money_capital += spend - salary
+    spend *= 1 + increase   # увеличиваем расходы на 3%
+    i += 1
+
+print(round(money_capital))
